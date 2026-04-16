1d3c52e4e730890e763f1a290f103d89a31217ea
Zunair Ahmad <125442598+Zunair20@users.noreply.github.com>
2026-04-16 15:04:07 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
-from flask import Flask, request
+import requests
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/data-example', methods=['GET', 'POST'])
-def handle_request():
-    name = request.args.get('name', 'Guest')
-
-    if request.method == 'POST':
-        form_data = request.form.get('username')
-        return f"Form submitted for: {form_data}"
-
-    return f"Hello, {name}! Send a POST request with JSON or Form data."
-
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/proxy')
+def proxy_request():
+    # Use 'requests' library to call an external API
+    response = requests.get('https://educative.io')
+    return response.json()
